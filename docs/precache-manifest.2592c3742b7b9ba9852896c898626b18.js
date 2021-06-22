93ce3741e087e51c053bf07657c8d3aa5b17963a
KateAstrid <63353519+KateAstrid@users.noreply.github.com>
2021-06-22 17:03:16 +0200

    next step

---
+++ docs/precache-manifest.2592c3742b7b9ba9852896c898626b18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35376b16b1fc25406d00a72a4337e212",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f2ff3d04f881f08aa61a",
+    "url": "./static/css/main.54f31f3d.chunk.css"
+  },
+  {
+    "revision": "92e5ba519112356bbf2e",
+    "url": "./static/js/2.40f895ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.40f895ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2ff3d04f881f08aa61a",
+    "url": "./static/js/main.dce6216c.chunk.js"
+  },
+  {
+    "revision": "0d2e8921fdca6c44b4d5",
+    "url": "./static/js/runtime-main.6609d8e0.js"
+  }
+]);
---
+++ docs/precache-manifest.90fc11a521404efc7ce901e5caee3d94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6fcf367ab1a52ec16e392b3d09b20aae",
+    "url": "./index.html"
+  },
+  {
+    "revision": "425bd22d9dafd592f13a",
+    "url": "./static/css/main.1d7b6877.chunk.css"
+  },
+  {
+    "revision": "92e5ba519112356bbf2e",
+    "url": "./static/js/2.40f895ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.40f895ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "425bd22d9dafd592f13a",
+    "url": "./static/js/main.69fbf1dc.chunk.js"
+  },
+  {
+    "revision": "0d2e8921fdca6c44b4d5",
+    "url": "./static/js/runtime-main.6609d8e0.js"
+  }
+]);
