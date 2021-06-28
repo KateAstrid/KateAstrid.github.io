83579a0833b901d20e9f9158a7170e67495e9bad
KateAstrid <63353519+KateAstrid@users.noreply.github.com>
2021-06-28 19:17:26 +0200

    some rewriting and corrections

--- src/App.js
+++ src/App.js
@@ -25,9 +25,9 @@ const LeftTitle = ({ title }) => {
 };
 
 const generalDescription = [
-    ['Amsterdam, The Netherlands'],
-    ['rational.astrid@gmail.com'],
-    ['+31620681510'],
+    'Amsterdam, The Netherlands',
+    'rational.astrid@gmail.com',
+    '+31620681510',
 ]
 
 const GeneralDescription = () => {
@@ -35,7 +35,7 @@ const GeneralDescription = () => {
         <React.Fragment>
             {generalDescription.map(element => {
                 return (
-                    <React.Fragment key="element">
+                    <React.Fragment key={element}>
                         <div className="line_description">{element}</div>
                     </React.Fragment>
                 )
@@ -82,7 +82,7 @@ const ListOfEducation = () => {
         <ul className="education_list">
             {education.map(([year, degree]) => {
                 return (
-                    <div className="education_row" key="year">
+                    <div className="education_row" key={year}>
                         <li className="grey">{year}</li>
                         <div className="degree">{degree}</div>
                     </div>
@@ -97,27 +97,41 @@ const RightColumn = () => {
         <div className="right_column">
             <div className="section_title">
                 <FontAwesomeIcon icon={faUserAlt} className="icon_profile"/>
-                <div>PROFILE</div>
+                <div>ABOUT ME</div>
             </div>
 
             <div className="main_text">
-                <div>From 2011 to 2015, I worked in the area of land cadastre, cartography and geodesy: I was involved in digitizing maps and building 
-                    multi-storey monolithic buildings.
-                    After that, I changed my profile to fashion photography and until December 2019 worked closely with it. An internal 
-                    engineer with understanding of composition, science of color and art
-                    pushed me to change my profession towards programming at the beginning of January 2020.
-                    Since then I have been studying a growing stack of front end technologies every day.
+                <div>From 2011 to 2015, I worked in the area of land cadastre, cartography, and geodesy. 
+                    I was involved in digitizing maps and constructing multi-story monolithic buildings, 
+                    working on site as a geodesist. I enjoyed the work and how useful it felt to society and hoped to 
+                    make a great career there. But unfortunately, the construction industry in Russia turned out to be 
+                    heavy with corruption and sexism, so I decided to change my profession. I always enjoyed physics 
+                    and applying it to the world, so I decided to put my knowledge of optics and natural phenomena to 
+                    become a photographer.
                 </div>
-                <div className="description">Why is it worth taking me to work despite the fact that I do not have a lot of experience?</div>
+                <div className="description">I taught myself all main photography principles such as color, light, 
+                    and composition, and worked as a fashion photographer for five years until December 2019. 
+                    My works were featured in magazines, and photography exhibitions in Europe. 
+                    Despite that, further growth in photography depended too much on the subjective tastes of people 
+                    and connections, and too little on applying knowledge to produce a great picture. 
+                    A software engineer friend has inspired me to apply my knowledge of science and visual arts to a 
+                    perfect fit: frontend development.
+                </div>
+                <div className="description">Starting from January 2020, I have been studying a growing stack of 
+                    frontend technologies every day. By August I've already taught myself enough JavaScript, React, 
+                    Redux, and other libraries and tools to get my first job as a middle software engineer. 
+                    Since then I have learned a lot about real world codebases and team work.
+                </div>
+                <div className="description">Despite not having a lot of experience in IT, 
+                    I have acquired a lot of desirable work qualities on my career journey:
+                </div>
                 <ul>
-                    <li>My weaknesses are the inability to stop working and increased discipline.</li>
-                    <li>I am in love with studying, and I am planning to do it until the end of my days gradually becoming 
-                        an increasingly valuable employee.</li>
-                    <li>I have a calm attitude to any features of a human nature.</li>
+                    <li>coming from building construction, where mistakes may cost people lives, 
+                        I have a high level of responsibility and discipline;</li>
+                    <li>working with individuals while being a photographer taught me how to communicate effectively, 
+                        make people comfortable, and understand their requirements, which is extremely valuable when 
+                        collaborating with team members in IT.</li>
                 </ul>
-                <div>Being person, who likes to make things efficient, functional, and beautiful, I am intrested in IT and everything in its orbit.
-                    Knowledge of mathematics, the scientific method and the desire to be a cool programmer help me with it.
-                </div>
             </div>
 
             <div className="section_title">
@@ -133,20 +147,22 @@ const RightColumn = () => {
                 <div className="grey job_date">09.2020-12.2020</div>
                 <div className="description">"Opora" LLC is a leader in producing and selling medical equipment in Russia.</div>
                 <div className="description">As a first job, this vacancy turned out to be a tough nut to crack. 
-                    It was assumed that I would work with React, but in fact all the code was written on PHP+VueJs, 
-                    which I had to study in the process. During my time at the company, 
-                    I converted all SASS to SCSS; refactored half of the existing front end base; 
-                    succesfully proposed to change the OS from Windows to Linux in the entire development department; 
-                    found and fixed multiple preexisting production bugs, including one which made impossible to buy goods through the mobile version of the site. 
-                    I also suggested to work on technical debt and to use more consistent agile, such as 
-                    brainstorming big challenges and pair programming.
+                    I was supposed to work only on frontend in React, but in fact all frontend code was written in Vue.js 
+                    templates which were generated by a PHP backend <span role="img" aria-label="face">😑</span>. I had to study a lot in progress including reading 
+                    and occasionally modifying backend as well. During my time at the company, 
+                    I converted all SASS to SCSS; refactored half of the existing frontend codebase while working on 
+                    related tasks; successfully proposed to change the OS from Windows to Linux in the entire development 
+                    department; found and fixed multiple pre-existing production bugs, including one which made it 
+                    impossible to buy anything through the mobile version of the site. I also suggested to work on 
+                    technical debt and to use more consistent Agile practices, such as properly planning big tasks and 
+                    pair programming.
                 </div>
                 <div className="description">My core activities included:
                     <ul>
-                        <li>development and maintenance front end of two company sites - the first for legal entities, 
-                            the second for individuals;</li> 
-                        <li>helping with PHP back end;</li>
-                        <li>work with legacy code. </li> 
+                        <li>development and maintenance front end of two company sites - one for legal entities, one for individuals;</li> 
+                        <li>helping with PHP backend;</li>
+                        <li>improving internal admin site;</li>
+                        <li>rewriting legacy code. </li> 
                     </ul>
                 </div>
 
@@ -155,12 +171,15 @@ const RightColumn = () => {
                     <div className="grey job_city">Moscow, Russia</div>
                 </div>
                 <div className="grey job_date">01.2021-05.2021</div>
-                <div className="description">"SmartAn" LLC is the first startup company in Russia for aggregation data for risk managers.</div>
-                <div className="description">Being the only front end developer in a company, I created from scratch front end 
-                    for the platform right up to the platform's entry to the American accelerator. I have implemented all the required 
-                    libraries, including Redux and in18n; integrated the project with the backend using axios; 
-                    and also helped backend developers to catch bugs and suggested correction of design projects to better 
-                    comply with the principles of UX/UI
+                <div className="description">"SmartAn" LLC is the first startup company in Russia which 
+                    aggregates data for risk managers.  </div>
+                <div className="description">Being the only frontend developer in the company, I created a 
+                    frontend for the platform from scratch right up to the platform's entry to the American accelerator. 
+                    The site was a single page application built with React and Redux using customized MaterialUI 
+                    components. It’s purpose was to display complex financial information so I have learned a lot 
+                    implementing the required pages and components. I have also supported internationalization, 
+                    integrated it with HTTP backend, made proposals to backend developers to make API more consistent, 
+                    and suggested design corrections to better comply with principles of UX/UI.
                 </div>
             </div>
 
