ef94e64f9b0c4c16decbe2858ecc10a171b237dd
Kate Astrid <kateastrid@Kates-MacBook-Pro.local>
2022-04-08 16:30:19 +0200

    next step

---
+++ build/precache-manifest.af3a820d5af88cfee2372e4e62693e3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "348ce7b8b99bcb2858210f133554fa55",
+    "url": "./index.html"
+  },
+  {
+    "revision": "06d9ec2ab15ac37f34a1",
+    "url": "./static/css/main.c582c069.chunk.css"
+  },
+  {
+    "revision": "ae92e3fc2559d35072a7",
+    "url": "./static/js/2.9009f322.chunk.js"
+  },
+  {
+    "revision": "e79898a42ec2d02d5bbd03509f17e33d",
+    "url": "./static/js/2.9009f322.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06d9ec2ab15ac37f34a1",
+    "url": "./static/js/main.55f11e04.chunk.js"
+  },
+  {
+    "revision": "0d2e8921fdca6c44b4d5",
+    "url": "./static/js/runtime-main.6609d8e0.js"
+  }
+]);
