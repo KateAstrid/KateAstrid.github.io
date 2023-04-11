0d02166e5e88ec52e4b7e13b9fe6c573e87c316c
kate.astrid <kate.astrid@superside.com>
2023-04-11 17:07:52 +0200

    next step of cv

---
+++ build/precache-manifest.78f8f32dc3c1409b9fa02a67bc5f8bda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e37e7ea968c1278cc0d3127841d6b53e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c23f7e1f6b82b3a62942",
+    "url": "./static/css/main.bd106cba.chunk.css"
+  },
+  {
+    "revision": "ae92e3fc2559d35072a7",
+    "url": "./static/js/2.9009f322.chunk.js"
+  },
+  {
+    "revision": "e79898a42ec2d02d5bbd03509f17e33d",
+    "url": "./static/js/2.9009f322.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c23f7e1f6b82b3a62942",
+    "url": "./static/js/main.f1478429.chunk.js"
+  },
+  {
+    "revision": "0d2e8921fdca6c44b4d5",
+    "url": "./static/js/runtime-main.6609d8e0.js"
+  }
+]);
